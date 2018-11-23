15c6cf7d169c52182f2d290851ef0ab7eced3c39
sdik <ahmadshiddiq0@gmail.com>
2018-11-23 14:00:36 +0700

    Initial Commit

---
+++ introzApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  introzApp
+//
+//  Created by ahmad shiddiq on 23/11/18.
+//  Copyright © 2018 ahmad shiddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
